89f84498dd98812d7377e0172ce7868a2260cf55
Emily Christy <Emily.Christy@miller-insurance.com>
2025-05-15 17:39:19 +0100

    Fixed type issue in product grid

--- src/features/products/components/Products-grid.tsx
+++ src/features/products/components/Products-grid.tsx
@@ -2,7 +2,7 @@
 
 import { Spinner } from "@/components/ui/spinner";
 // import { formatDate } from '@/utils/format';
-
+import { Product } from "@/types/api";
 import { useProducts } from "../api/get-products";
 
 // const searchParams = useSearchParams();
@@ -21,8 +21,8 @@ export const ProductsGrid = () => {
       </div>
     );
   }
-  const products = productsQuery.data;
-  console.log(products, "productsQuery data");
+
+  const products = productsQuery.data as Product[] | undefined;
 
   if (!products) return null;
 
@@ -31,11 +31,14 @@ export const ProductsGrid = () => {
       id="products-grid"
       className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5"
     >
-      {products?.map((product) => {
+      {products?.map((product: Product) => {
         // const LinkIcon = link.icon;
         return (
           <>
-            <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+            <div
+              key={product.id}
+              className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+            >
               <a href="#">
                 <img
                   src="https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
@@ -58,22 +61,7 @@ export const ProductsGrid = () => {
                         $199
                       </p>
                     </del>
-                    <div className="ml-auto">
-                      {/* <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        fill="currentColor"
-                        className="bi bi-bag-plus"
-                        viewBox="0 0 16 16"
-                      >
-                        <path
-                          fillrule="evenodd"
-                          d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
-                        />
-                        <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                      </svg> */}
-                    </div>
+                    <div className="ml-auto"></div>
                   </div>
                 </div>
               </a>
