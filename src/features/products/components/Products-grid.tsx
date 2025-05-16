b725ac5dbec346768a09eb595479cf1eae31e81a
Emily Christy <Emily.Christy@miller-insurance.com>
2025-05-16 14:54:11 +0100

    Updated next version and fixed key problem with grid

--- src/features/products/components/Products-grid.tsx
+++ src/features/products/components/Products-grid.tsx
@@ -35,39 +35,37 @@ export const ProductsGrid = () => {
         // const LinkIcon = link.icon;
 
         return (
-          <>
-            <div
-              key={product.id}
-              className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
-            >
-              <a href="#">
-                <img
-                  src="https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                  alt="Product"
-                  className="h-80 w-72 object-cover rounded-t-xl"
-                />
-                <div className="px-4 py-3 w-72">
-                  <span className="text-gray-400 mr-3 uppercase text-xs">
-                    Brand
-                  </span>
-                  <p className="text-lg font-bold text-black truncate block capitalize">
-                    {product.title}
+          <div
+            key={product.id}
+            className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+          >
+            <a href="#">
+              <img
+                src="https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                alt="Product"
+                className="h-80 w-72 object-cover rounded-t-xl"
+              />
+              <div className="px-4 py-3 w-72">
+                <span className="text-gray-400 mr-3 uppercase text-xs">
+                  Brand
+                </span>
+                <p className="text-lg font-bold text-black truncate block capitalize">
+                  {product.title}
+                </p>
+                <div className="flex items-center">
+                  <p className="text-lg font-semibold text-black cursor-auto my-3">
+                    $149
                   </p>
-                  <div className="flex items-center">
-                    <p className="text-lg font-semibold text-black cursor-auto my-3">
-                      $149
+                  <del>
+                    <p className="text-sm text-gray-600 cursor-auto ml-2">
+                      $199
                     </p>
-                    <del>
-                      <p className="text-sm text-gray-600 cursor-auto ml-2">
-                        $199
-                      </p>
-                    </del>
-                    <div className="ml-auto"></div>
-                  </div>
+                  </del>
+                  <div className="ml-auto"></div>
                 </div>
-              </a>
-            </div>
-          </>
+              </div>
+            </a>
+          </div>
         );
       })}
     </div>
